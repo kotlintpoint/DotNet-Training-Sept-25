fb51fd64b592ade930b9ad95598c64f9968d929d
Kotlintpoint <ankit.sodha123@gmail.com>
2025-09-17 09:34:35 +0530

    Day1
    Variable, Function, Public, Private, Scope, Static, Logical Flow, int.TryParse(), string.IsNullOrEmpty, String.Equals()

---
+++ ConsoleHelloWorld/ConsoleHelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleHelloWorld;
+
+Console.WriteLine("Hello, World!");
+
+String str = "Hello From Console Application!!!";
+
+Console.WriteLine("Welcome " + str);
+
+Student student = new Student();
+student.HelloStudent();
+student.Sum(-10, 20);
+student.Sum(10, 20);
+//String studentName = student.getName();
+//Console.WriteLine($"Student: {studentName}");
+
+Console.WriteLine($"Student: {student.getName()}");
+
+// static members can be accessed directly without creating object
+// Single memory allocation for static members 
+Student.WelcomeStudent();
+
+const int num = 100;
+//num = 200;
+
+Student.doMultiplication();
+
+object name = "String";
+string user = typeof(string).Name;
+Console.WriteLine($"{name} == {user} : {name == user}");
+Console.WriteLine($"{name} Equals {user} : {name.Equals(user)}");
+
+
---
+++ ConsoleHelloWorld/ConsoleHelloWorld/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHelloWorld
+{
+    internal class Student
+    {
+        public static void WelcomeStudent() {
+            Console.WriteLine("Welcome Student");
+        }
+
+        public void HelloStudent() {
+            Console.WriteLine("Hello Student");
+        }
+
+        public void Sum(int no1, int no2) {
+            if (no1 < 0 || no2 < 0)
+            {
+                Console.WriteLine("Negative numbers Error.");
+                return;
+            }
+            Console.WriteLine("Sum = " + (no1 + no2));
+        }
+
+        public String getName() {
+            Console.Write("Enter Name : ");
+            String name = Console.ReadLine()!;
+            return name;
+        }
+
+        static int readNumber()
+        {
+            Console.Write("Enter Number : ");
+            int num;
+            bool isNumber = int.TryParse(Console.ReadLine()!, out num);
+            if (isNumber)
+            {
+                return num;
+            }
+            Console.WriteLine("Please Enter number...");
+            return 0;
+        }
+
+        public static void doMultiplication() {            
+            int num1 = readNumber();
+            int num2 = readNumber();
+            int mul = num1 * num2;
+            Console.WriteLine($"Multiplication : {mul}");
+        }
+    }
+}
