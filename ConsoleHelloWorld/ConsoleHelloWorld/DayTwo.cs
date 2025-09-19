47a7205e4447c0d4e27da76298e45d7b72472063
Kotlintpoint <ankit.sodha123@gmail.com>
2025-09-19 09:28:54 +0530

    Loop, Array Debugging, string

---
+++ ConsoleHelloWorld/ConsoleHelloWorld/DayTwo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHelloWorld
+{
+    internal class DayTwo
+    {
+        public static void changeNumber(int num) {
+            num = 0;
+        }
+
+        public static void changeArray(int[] array)
+        {
+            array[0] = 0;
+            array[1] = 0;
+            array[2] = 0;
+        }
+
+        public static void printFibonacci(int range) {
+            int num1 = 0, num2 = 1;
+            if (range == 1) {
+                Console.Write($"{num1}  ");
+                return;
+            }
+            Console.Write($"{num1}  {num2}  ");
+            if (range > 2) {
+                int sum = num1 + num2;
+                Console.Write($"{sum}  ");
+                for (int i = 4; i <= range; i++) {
+                    num1 = num2;
+                    num2 = sum;
+                    sum = num1 + num2;
+                    Console.Write($"{sum}  ");
+                }
+            }            
+        }
+    }
+}
--- ConsoleHelloWorld/ConsoleHelloWorld/Program.cs
+++ ConsoleHelloWorld/ConsoleHelloWorld/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleHelloWorld;
 
+/*
 Console.WriteLine("Hello, World!");
 
 String str = "Hello From Console Application!!!";
@@ -29,5 +30,61 @@ object name = "String";
 string user = typeof(string).Name;
 Console.WriteLine($"{name} == {user} : {name == user}");
 Console.WriteLine($"{name} Equals {user} : {name.Equals(user)}");
+*/
+
+// Day Two
+
+// Primitive Type 
+var num = 100;
+DayTwo.changeNumber(num);   // call by value
+Console.WriteLine($"num = {num}");
+
+// Reference type
+var arrayBool = new bool[3];
+Console.WriteLine("Before Initialization Boolean Array");
+Console.WriteLine($"{arrayBool[0]}, {arrayBool[1]}, {arrayBool[2]}");
+
+var array = new int[3];
+Console.WriteLine("Before Initialization");
+Console.WriteLine($"{array[0]}, {array[1]}, {array[2]}");
+
+Console.WriteLine("After Initialization");
+array[0] = 1;
+array[1] = 2;
+array[2] = 3;
+Console.WriteLine($"{array[0]}, {array[1]}, {array[2]}");
+Console.WriteLine("After Call by Reference");
+DayTwo.changeArray(array);     // call by reference 
+
+Console.WriteLine($"{array[0]}, {array[1]}, {array[2]}");
+Console.WriteLine($"Length : {array.Length}");
 
 
+var students = new Student[3];
+Console.WriteLine($"{students[0]}, {students[1]}, {students[2]}");
+
+
+for (var i = 0; i < array.Length; i++)
+{
+    Console.Write("Enter number : ");
+    int.TryParse(Console.ReadLine(), out array[i]);
+}
+
+foreach (var number in array)
+{
+    Console.Write($"{number}, ");
+}
+
+var name = "tops technology";
+//name[0] = 'T';
+Console.WriteLine(name[0]);
+Console.WriteLine(char.ToUpper(name[0]) + name.Substring(1));
+
+Console.WriteLine();
+//DayTwo.printFibonacci(1);
+//Console.WriteLine();
+//DayTwo.printFibonacci(2);
+//Console.WriteLine();
+//DayTwo.printFibonacci(3);
+//Console.WriteLine();
+DayTwo.printFibonacci(10);
