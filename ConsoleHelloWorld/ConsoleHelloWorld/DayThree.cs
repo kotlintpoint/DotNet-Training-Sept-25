cea12885386498109b53f15c68ecb026fb664d6b
Kotlintpoint <ankit.sodha123@gmail.com>
2025-09-22 09:22:03 +0530

    String, StringBuilder, Substring, Join, Replace, Compare, Split, Sorting
    File Read, File Write, Dispose Unmanaged Resource, StreamWriter, StreamReader

---
+++ ConsoleHelloWorld/ConsoleHelloWorld/DayThree.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHelloWorld
+{
+    internal class DayThree
+    {
+        public static void CompareStringAndStringBuilder() {
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var sb = new StringBuilder();
+            for (var i = 1; i <= 100000; i++) {
+                sb.Append("Append String");
+            }
+            sw.Stop();
+            var elapsed1 = sw.ElapsedMilliseconds;
+            sw.Reset();
+
+            sw.Start();
+            var str = "";
+            for (var i = 1; i <= 100000; i++)
+            {
+                str += "Test";
+            }
+            sw.Stop();
+            var elapsed2 = sw.ElapsedMilliseconds;
+
+            Console.WriteLine($"StringBuilder elapsed1 is {elapsed1} and String elapsed2 is {elapsed2}");
+        }
+
+        public static void DemoSubstring() {
+            var word = "Hello World!";
+
+            string result;
+            result = word.Substring(6);
+            Console.WriteLine($"word.Substring(6) = {result}");
+            result = word.Substring(0, 5);
+            Console.WriteLine($"word.Substring(0, 5) = {result}");
+            result = word[..5];
+            Console.WriteLine($"word[..5] = {result}");
+            result = word[6..];
+            Console.WriteLine($"word[6..] = {result}");
+            result = word[..^1];    // word.Length - 1
+            Console.WriteLine($"word[..^1] = {result}");
+            result = word[^2..];
+            Console.WriteLine($"word[^2..] = {result}");
+            result = word[1..^1];
+            Console.WriteLine($"word[1..^1] = {result}");
+
+            var numbers = new int[] { 1, 2, 3 };
+            var lessNumbers = numbers[..^1];
+            foreach (var num in lessNumbers) {
+                Console.WriteLine($"{num}");
+            }
+
+        }
+
+        public static void CompareToDemo() {
+            var a = "a";
+            var b = "b";
+            Console.WriteLine($"a.CompareTo(b) => {a.CompareTo(b)}");
+            Console.WriteLine($"b.CompareTo(a) => {b.CompareTo(a)}");
+            Console.WriteLine($"a.CompareTo(a) => {a.CompareTo(a)}");
+
+            var cities = new string[] { "Surat", "Ahmedabad", "Baroda", "Navsari" };
+            // alphabetical order sorting
+            for (var i = 0; i < cities.Length; i++) {
+                for (var j = i + 1; j < cities.LongLength; j++) {
+                    if (cities[i].CompareTo(cities[j]) > 0) {
+                        var temp = cities[i];
+                        cities[i] = cities[j];
+                        cities[j] = temp;
+                    }
+                }
+            }
+            foreach (var city in cities) {
+                Console.WriteLine(city);
+            }
+
+            var citiesString = string.Join(".", cities);
+            Console.WriteLine($"citiesString => {citiesString}");
+            citiesString = citiesString.Replace(".", ", ");
+            Console.WriteLine($"citiesString => {citiesString}");
+            var newCities = citiesString.Split(", ");
+            foreach (var city in newCities) { Console.WriteLine(city); }
+        }
+
+        public static void WriteFiles() {
+            File.WriteAllText(@"F:\Documents\test.txt", "Hello World!!!!!");
+            Console.WriteLine("File is Written!!!");
+        }
+
+        public static void ReadFile() {
+            var data = File.ReadAllText(@"F:\Documents\test.txt");
+            Console.WriteLine($"{data}");
+        }
+
+        public static void StreamReaderWriter() {
+            // Get the directories currently on the C drive.
+            DirectoryInfo[] cDirs = new DirectoryInfo(@"c:\").GetDirectories();
+
+            // Write each directory name to a file.
+            using (StreamWriter sw = new StreamWriter("CDriveDirs.txt"))
+            {
+                foreach (DirectoryInfo dir in cDirs)
+                {
+                    sw.WriteLine(dir.Name);
+                }
+            }
+
+            // Read and show each line from the file.
+            string? line = "";
+            using (StreamReader sr = new StreamReader("CDriveDirs.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
--- ConsoleHelloWorld/ConsoleHelloWorld/Program.cs
+++ ConsoleHelloWorld/ConsoleHelloWorld/Program.cs
@@ -35,56 +35,67 @@ Console.WriteLine($"{name} Equals {user} : {name.Equals(user)}");
 // Day Two
 
 // Primitive Type 
-var num = 100;
-DayTwo.changeNumber(num);   // call by value
-Console.WriteLine($"num = {num}");
+//var num = 100;
+//DayTwo.changeNumber(num);   // call by value
+//Console.WriteLine($"num = {num}");
 
-// Reference type
-var arrayBool = new bool[3];
-Console.WriteLine("Before Initialization Boolean Array");
-Console.WriteLine($"{arrayBool[0]}, {arrayBool[1]}, {arrayBool[2]}");
+//// Reference type
+//var arrayBool = new bool[3];
+//Console.WriteLine("Before Initialization Boolean Array");
+//Console.WriteLine($"{arrayBool[0]}, {arrayBool[1]}, {arrayBool[2]}");
 
-var array = new int[3];
-Console.WriteLine("Before Initialization");
-Console.WriteLine($"{array[0]}, {array[1]}, {array[2]}");
+//var array = new int[3];
+//Console.WriteLine("Before Initialization");
+//Console.WriteLine($"{array[0]}, {array[1]}, {array[2]}");
 
-Console.WriteLine("After Initialization");
-array[0] = 1;
-array[1] = 2;
-array[2] = 3;
-Console.WriteLine($"{array[0]}, {array[1]}, {array[2]}");
-Console.WriteLine("After Call by Reference");
-DayTwo.changeArray(array);     // call by reference 
+//Console.WriteLine("After Initialization");
+//array[0] = 1;
+//array[1] = 2;
+//array[2] = 3;
+//Console.WriteLine($"{array[0]}, {array[1]}, {array[2]}");
+//Console.WriteLine("After Call by Reference");
+//DayTwo.changeArray(array);     // call by reference 
 
-Console.WriteLine($"{array[0]}, {array[1]}, {array[2]}");
-Console.WriteLine($"Length : {array.Length}");
-
-
-var students = new Student[3];
-Console.WriteLine($"{students[0]}, {students[1]}, {students[2]}");
+//Console.WriteLine($"{array[0]}, {array[1]}, {array[2]}");
+//Console.WriteLine($"Length : {array.Length}");
 
 
-for (var i = 0; i < array.Length; i++)
-{
-    Console.Write("Enter number : ");
-    int.TryParse(Console.ReadLine(), out array[i]);
-}
+//var students = new Student[3];
+//Console.WriteLine($"{students[0]}, {students[1]}, {students[2]}");
 
-foreach (var number in array)
-{
-    Console.Write($"{number}, ");
-}
 
-var name = "tops technology";
-//name[0] = 'T';
-Console.WriteLine(name[0]);
-Console.WriteLine(char.ToUpper(name[0]) + name.Substring(1));
+//for (var i = 0; i < array.Length; i++)
+//{
+//    Console.Write("Enter number : ");
+//    int.TryParse(Console.ReadLine(), out array[i]);
+//}
 
-Console.WriteLine();
-//DayTwo.printFibonacci(1);
+//foreach (var number in array)
+//{
+//    Console.Write($"{number}, ");
+//}
+
+//var name = "tops technology";
+////name[0] = 'T';
+//Console.WriteLine(name[0]);
+//Console.WriteLine(char.ToUpper(name[0]) + name.Substring(1));
+
 //Console.WriteLine();
-//DayTwo.printFibonacci(2);
-//Console.WriteLine();
-//DayTwo.printFibonacci(3);
-//Console.WriteLine();
-DayTwo.printFibonacci(10);
+////DayTwo.printFibonacci(1);
+////Console.WriteLine();
+////DayTwo.printFibonacci(2);
+////Console.WriteLine();
+////DayTwo.printFibonacci(3);
+////Console.WriteLine();
+//DayTwo.printFibonacci(10);
+
+
+// Day Three
+
+//DayThree.CompareStringAndStringBuilder();
+//DayThree.DemoSubstring();
+//DayThree.CompareToDemo();
+//DayThree.WriteFiles();
+//DayThree.ReadFile();
+DayThree.StreamReaderWriter();
+
