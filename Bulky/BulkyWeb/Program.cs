4148931de23233e32ab65ea9c3f12a80953de8ce
Kotlintpoint <ankit.sodha123@gmail.com>
2025-10-29 09:27:04 +0530

    Model, View, Controller, ApplicationDbContext, Link
    Add-Migration, Update-Database, Data Seeding

--- Bulky/BulkyWeb/Program.cs
+++ Bulky/BulkyWeb/Program.cs
@@ -1,7 +1,17 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException("Connection string"
+        + "'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
