2373d4340fa1983ea8e02f9eb31ee75900ff69e5
Kotlintpoint <ankit.sodha123@gmail.com>
2025-11-03 09:30:46 +0530

    Server Side Validation, client side validation, Edit, Update Category, TempData, toaster, partial view

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -27,8 +27,76 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category CategoryObj)
         {
-            _db.Categories.Add(CategoryObj);
+            if(CategoryObj.Name == CategoryObj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "DisplayOrder cannot be same as Name.");
+            }
+            if (CategoryObj.Name != null && CategoryObj.Name.ToLower() == "test") 
+            {
+                ModelState.AddModelError("", "test is an invalid value.");
+            }
+            if (ModelState.IsValid) 
+            {
+                _db.Categories.Add(CategoryObj);
+                _db.SaveChanges();
+                TempData["message"] = "Category created Successfully.";
+                return RedirectToAction("Index");
+            }            
+            return View();
+        }
+
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(Id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category CategoryObj)
+        {          
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(CategoryObj);
+                _db.SaveChanges();
+                TempData["message"] = "Category updated Successfully.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(Id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? Id)
+        {           
+            Category? categoryFromDb = _db.Categories.Find(Id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(categoryFromDb);
             _db.SaveChanges();
+            TempData["message"] = "Category deleted Successfully.";
             return RedirectToAction("Index");
         }
     }
